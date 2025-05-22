273a9b2d11f78a3b45693ddd6bfc7c0b4afbd8ce
Rachel Davis <rachelddavis@aol.com>
2025-05-22 07:53:54 -0600

    Unit3.SecretTunnel Updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useAuth } from "./AuthContext";
-
 import Entrance from "./Entrance";
-import Tablet from "./Tablet";
-import Tunnel from "./Tunnel";
+import Tablet from "./Tablet.jsx";
+import Tunnel from "./Tunnel.jsx";
 
-export default function App() {
+function App() {
   const { location } = useAuth();
-
   if (location === "GATE") return <Entrance />;
   if (location === "TABLET") return <Tablet />;
   return <Tunnel />;
 }
+
+export default App;
